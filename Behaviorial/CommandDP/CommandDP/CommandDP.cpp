1296679249e836632806e0283cc114c3ef33b12e
Vibhuti <vibhutipratapsingh@gmail.com>
2024-03-09 22:05:54 +0530

    Made some changes in command DP

--- Behaviorial/CommandDP/CommandDP/CommandDP.cpp
+++ Behaviorial/CommandDP/CommandDP/CommandDP.cpp
@@ -1,76 +1,95 @@
 #include <string>
 #include <iostream>
-#include <vector>
+#include <stack>
 #include <ctime>
 #include <cstdlib>
 
-struct Character {
+struct Character{
     int x;
     int y;
     std::string name;
-    Character(std::string _name) : name(_name) {
+    Character(std::string _name) : name(_name){
     }
-    void move(int _x, int _y) {
+    void move(int _x, int _y){
         x = _x;
         y = _y;
     }
 };
 
-class Command {
-public:
-    virtual ~Command() {}
-    virtual void execute(Character& c) = 0;
-    virtual void undo(Character& c) = 0;
+class Command{
+    public:
+    virtual ~Command(){}
+    virtual void execute() = 0;
+    virtual void undo() = 0;
 };
 
-class Move : public Command {
+class MoveCommand: public Command{
+    Character* characher_;
 public:
-    void execute(Character& c) {
-        int randomx = rand() % 4;
-        int randomy = rand() % 4;
-        c.move(randomx, randomy);
-        x = randomx;
-        y = randomy;
-        std::cout << c.name << " moved " << x << "," << y << std::endl;
+    MoveCommand(Character* characher) :characher_{ characher }
+    {}
+    void execute()
+    {
+        characher_->x = rand() %4;
+        characher_->y = rand() %4;
+        characher_->move(characher_->x, characher_->y);
+        std::cout << characher_->name << " moved " << characher_->x << "," << characher_->y << std::endl;
     }
-    void undo(Character& c) {
-        c.move(-x, -y);
-        std::cout << c.name << " undo " << -x << "," << -y << std::endl;
+    void undo(){
+        //characher_->move(characher_->x, characher_->y);
+        std::cout << characher_->name << " undo " << -characher_->x << "," << -characher_->y << std::endl;
     }
-private:
-    int x;
-    int y;
+};
+
+
+class Invoker {
+    std::stack<Command*> command_stack_;
+public:
+
+    void pressCommand(Command* command)
+    {
+        command->execute();
+        command_stack_.push(command);
+    }
+
+    void pressUndoCommand()
+    {
+        command_stack_.top()->undo();
+    }
+
+
 };
 
 
 
-int main() {
+int main(){
     srand(time(NULL));
 
-    Character mike("mike");;
-    std::vector<Command*> command_queue;
+    Character *character = new Character("mike");;
+    Command* c1 = new MoveCommand(character);
+    Invoker* inv = new Invoker();
+    inv->pressCommand(c1);
+    inv->pressUndoCommand();
+    //command_queue.push_back(m1);
+    //Move* m2 = new Move;
+    //command_queue.push_back(m2);
+    //Move* m3 = new Move;
+    //command_queue.push_back(m3);
+    //Move* m4 = new Move;
+    //command_queue.push_back(m4);
 
-    Move* m1 = new Move;
-    command_queue.push_back(m1);
-    Move* m2 = new Move;
-    command_queue.push_back(m2);
-    Move* m3 = new Move;
-    command_queue.push_back(m3);
-    Move* m4 = new Move;
-    command_queue.push_back(m4);
+    //for(int i=0; i < command_queue.size(); i++){
+    //    command_queue[i]->execute(mike); 
+    //}
 
-    for (int i = 0; i < command_queue.size(); i++) {
-        command_queue[i]->execute(mike);
-    }
+    //for(int i=command_queue.size()-1; i > -1; i--){
+    //    command_queue[i]->undo(mike); 
+    //}
 
-    for (int i = command_queue.size() - 1; i > -1; i--) {
-        command_queue[i]->undo(mike);
-    }
+    //// And of course some cleanup...
+    //for(int i=0; i < command_queue.size(); i++){
+    //    delete command_queue[i];
+    //}
 
-    // And of course some cleanup...
-    for (int i = 0; i < command_queue.size(); i++) {
-        delete command_queue[i];
-    }
-
-    return 0;
+    //return 0;
 }
